de7843a97579c3920389f4b89bf215d3dc7ff346
Roger Lysberg <roger@24nettbutikk.no>
2016-04-12 20:36:06 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,9 +12,23 @@ brew update
 # Install git
 brew install git
 
-# 
+# A place to store git repos
 mkdir $HOME/Git
 git clone https://github.com/rogerlysberg/macsetup.git $HOME/Git/macsetup.git
+# Incase we already have the git repo, try to update it
+cd $HOME/Git/macsetup.git
+git pull $HOME/Git/macsetup.git
 
 # Install all our dependencies with bundle (See Brewfile)
 brew tap homebrew/bundle
+
+# Install Composer
+curl -sS https://getcomposer.org/installer | php
+mv composer.phar /usr/local/bin/composer
+
+# Install global Composer packages
+/usr/local/bin/composer global require laravel/installer laravel/lumen-installer
+
+# Create a Sites directory
+# This is a default directory for OS X user accounts but doesn't comes pre-installed
+mkdir $HOME/Sites
