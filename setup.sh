055e04dc2d7d178f3c677cb1c967f1acf93b95d8
Roger Lysberg <roger@24nettbutikk.no>
2016-04-12 19:50:08 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-echo "Installing Howebrew"
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
+# Check for Homebrew and install if we don't have it
+if test ! $(which brew); then
+  echo "Installing Howebrew"
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
